9cf29cf4fb732736c7681a1c11d4024e544f95ca
Nick Evans <nick.evans42@gmail.com>
2015-06-15 14:40:08 -0600

    devise with omniauth, add bootstrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,22 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # General Settings
+  config.app_domain = 'lvh.me'
+  config.app_port = 3000
+
+  # Email
+  config.action_mailer.default_url_options = { host: config.app_domain, port: config.app_port }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: '587',
+    enable_starttls_auto: true,
+    user_name: 'yourgmail',
+    password: 'gmailpassword',
+    authentication: :plain,
+    domain: 'lvh.me'
+  }
 end
